10e30ceaaa692b8d6e924ad2feea3bc88fa2e4d8
Edi Zeqiri <89740646+edizeqiri@users.noreply.github.com>
2024-01-15 10:21:12 +0100

    updated tlsh

--- Analysis/tmp.py
+++ Analysis/tmp.py
@@ -1,8 +1,21 @@
-def compare_hashes(batch, full_array, col_index):
-    results = []
-    for item in batch:
-        for other_item in full_array:
-            if item[col_index] != other_item[col_index]:  # To avoid comparing with itself
-                diff_score = tlsh.diff(item[col_index], other_item[col_index])
-                results.append((item[0], other_item[0], item[1], other_item[1], diff_score))
-    return results
+import tlsh
+def subresult(frame):
+    existing_shm = shared_memory.SharedMemory(name=shm_name)
+#   numi = np.ndarray(shape, dtype=dtype, buffer=existing_shm.buf)
+    local_result = []
+    for i in range(len(frame)):
+        min_score = 10000
+        winner = []
+        score = 100000
+        for j in range(len(numi)):
+            if frame[i][3] == numi[j][3]:
+                continue
+            try:
+                score = tlsh.diff(frame[i][0], numi[j][0])
+            except:
+                print(f"Error in tlsh: {frame[i][1]} and {numi[j][1]}, {frame[i][2]} and {numi[j][2]}, ")
+            if score < min_score:
+                min_score = score
+                winner = [frame[i][1], numi[j][1], frame[i][2], numi[j][2], score]
+        local_result.append(winner)
+    return local_result
